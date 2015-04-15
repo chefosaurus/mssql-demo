7bd58d8e8a67b0726fba6c6857b7f2ff8bf65a5d
Aimee Foster <afoster@chef.io>
2015-04-15 15:26:37 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# mssql-demo
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'mssql-demo'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures mssql-demo'
+long_description 'Installs/Configures mssql-demo'
+version          '0.1.0'
+
