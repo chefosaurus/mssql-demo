32bc5273612df66081183233d71e17e8ff1295fe
Sean Carolan <scarolan@gmail.com>
2015-04-15 20:52:42 -0500

    Added Pester tests for webserver recipe.

---
+++ test/integration/default/pester/default.tests.ps1
@@ -0,0 +1,20 @@
+describe 'iis::default'  {
+    It "Should have the Web Server Role" { 
+      (Get-WindowsFeature -name Web-Server).Installed | Should Be $true # Probably not the right service name but it's okay for now.
+    }
+
+    It "Should have World Wide Web Publishing Service running" {
+      (Get-Service -name 'W3SVC').status | Should Be 'Running'
+      (Get-WmiObject win32_service | Where {$_.Name -eq "W3SVC"}).StartMode | Should Be "Auto"
+    }
+
+    It "Should have a default website" {
+      Get-Website -Name "Default Web Site" | Should be $true
+      (Get-Website -Name "Default Web Site").State | Should be "Started"
+    }
+    # We need to figure out a way to enssure that the app pool is bound to the site, etc, but this is a start
+    It "Should have a default app pool" {
+     Get-WebAppPoolState -Name "DefaultAppPool" | Should be $true
+     (Get-WebAppPoolState -Name "DefaultAppPool").value | Should be "Started"
+    }
+}
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'mssql-demo::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
+  #it 'does something' do
+  #  skip 'Replace this with meaningful tests'
+  #end
 end
